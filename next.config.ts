7cbbb9ac9b1d445440d9253002d72565a638ef0a
parampanwar <parampanwar36@gmail.com>
2024-11-15 16:42:56 +0530

    Fix Netlify 404 issue

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ // next.config.js
+  module.exports = {
+  output: 'standalone',
+},
 };
 
 export default nextConfig;
